1b6d552ee16314f60c7cece0e82e30ed825bcbb5
اسمك هنا < >
2026-01-04 14:06:45 +0300

    it was a tiring day

--- app/Auth/Login.tsx
+++ app/Auth/Login.tsx
@@ -1,62 +1,115 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import { useState } from 'react'
 import { Button, TextInput } from 'react-native-paper';
+import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Login = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [error, setError] = useState<string>('');
+    
+    const handleLogin = () => {
+      if(email && password) {
+        console.log('Email:', email);
+        console.log('Password:', password);
+        console.log('Login successful!');
+      }
+      if(!email || !password) {
+        setError('Please enter email and password');
+        return;
+      }
+      if(password.length < 6) {
+        setError('Password must be at least 6 characters long');
+        return;
+      }
+    }
   return (
-    <View style={styles.container}>
-        <Image
-        source={require("@/assets/images/noteLogin.png")}
-        style={{ width: 100, height: 100, marginBottom: 50 }}
-         />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+          <Image
+          source={require("@/assets/images/noteLogin.png")}
+          style={{ width: 100, height: 100, marginBottom: 30, marginTop:30 }}
+          />
+          <Text style={{textAlign: 'center', marginBottom: 20}}>تنبيه ! تسجيل الدخول ليس حقيقيا</Text>
+          <Text style={styles.title}>تسجيل الدخول</Text>
         
-        <Text style={styles.title}>تسجيل الدخول</Text>
-      
-        <TextInput
-        label="البريد الالكتروني"
-        onChangeText={setEmail}
-        value={email}
-        placeholder='example@gmail.com'
-        mode="outlined"
-        outlineColor="#E0E0E0"
-        activeOutlineColor="#333"
-        theme={{ roundness: 12 }}
-        style={styles.inputs}
-        error={!!error}
-        
+          <TextInput
+          label="البريد الالكتروني"
+          onChangeText={setEmail}
+          value={email}
+          keyboardType='email-address'
+          placeholder='example@gmail.com'
+          mode="outlined"
+          outlineColor="#E0E0E0"
+          activeOutlineColor="#333"
+          theme={{ roundness: 12 }}
+          style={styles.inputs}
+          error={!!error}
+          
+          />
+          
+          <TextInput
+          label="كلمة المرور"
+          onChangeText={setPassword}
+          value={password}
+          mode="outlined"
+          placeholder='أدخل كلمة المرور'
+          secureTextEntry
+          outlineColor="#E0E0E0"
+          activeOutlineColor="#333"
+          theme={{ roundness: 12 }}
+          style={styles.inputs}
+          error={!!error}
         />
         
-        <TextInput
-        label="كلمة المرور"
-        onChangeText={setPassword}
-        value={password}
-        mode="outlined"
-        placeholder='أدخل كلمة المرور'
-        secureTextEntry
-        outlineColor="#E0E0E0"
-        activeOutlineColor="#333"
-        theme={{ roundness: 12 }}
-        style={styles.inputs}
-        error={!!error}
-        />
-        
-        <Button
-        mode="contained"
-        style={{width: 300, borderRadius: 20, elevation: 10}}
-        buttonColor='#6A6AD2'
-        textColor='#fff'
-        onPress={() => console.log('Login successful!')}
+        {error && <Text style={{ color: 'red', marginBottom: 10 }}>{error}</Text>}
+          
+          <TouchableOpacity onPress={handleLogin} style={styles.login}>
+            <Image
+            source={require("@/assets/images/user.png")}
+            style={{ width: 20, height: 20, marginRight: 10, tintColor: '#fff' }}
+            />
+            <Text style={styles.loginText}>تسجيل الدخول</Text>
+          </TouchableOpacity>
+          
+          <Button
+          mode="text"
+          textColor='#000'
+          onPress={() => router.replace('./Signup')}
+          style={styles.New}>ليس لديك حساب؟</Button>
+          
+          <View style={styles.deviderContainer}>
+            <View  style={styles.devider}/>
+            <Text style={styles.or}>أو</Text>
+            <View  style={styles.devider}/>
+          </View>
+          
+        <TouchableOpacity
+        style={styles.icons}
         >
-            تسجيل الدخول
-        </Button>
-        
-        <Text style={styles.New}>Create a new account ?</Text>
-        
-        <Text></Text>
-    </View>
+          <Image
+          source={require("@/assets/images/google.png")}
+          style={{ width: 30, height: 30, marginRight: 10 }}
+          />
+          <Text style={styles.iconText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.icons}>
+          <Image
+          source={require("@/assets/images/facebook.png")}
+          style={{ width: 30, height: 30, marginRight: 10 }}
+          />
+          <Text style={styles.iconText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.icons}>
+          <Image
+            source={require("@/assets/images/twitter.png")}
+            style={{ width: 30, height: 30, marginRight: 10 }}
+          />
+          <Text style={styles.iconText}>Continue with X/Twitter</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -80,9 +133,52 @@ const styles = StyleSheet.create({
       borderRadius: 20,
       backgroundColor: '#EFEFEF'
     },
+    login:{
+      flexDirection:'row',
+      width: 300,
+      alignItems: 'center',
+      borderRadius: 20,
+      justifyContent: 'center',
+      backgroundColor: '#3A7BD5',
+      padding: 10
+    },
+    loginText: {
+      fontSize: 16,
+      color: '#fff',
+      textAlign: 'center'
+    },
     New: {
       fontSize: 14,
       color: '#000',
-        marginTop: 15
-    }
+      marginTop: 10
+    },
+  deviderContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 24
+  },
+  devider: {
+    flex: 1,
+    height: 2,
+    backgroundColor: '#010101'
+  },
+  or: {
+    marginHorizontal: 8,
+    fontSize: 16
+  },
+  icons: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginBottom: 10,
+    width: 300,
+    gap: 10,
+    backgroundColor: "#FFFFFF",
+    padding: 10,
+    borderRadius: 10
+  },
+  iconText: {
+    fontSize: 14,
+    justifyContent: "center"
+  }
 })
--- app/Auth/Signup.tsx
+++ app/Auth/Signup.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
 const Signup = () => {
   return (
-    <View style={styles.container}>
-      <Text>signup</Text>
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text>signup</Text>
+      </View>
+    </SafeAreaView>
+    
   )
 }
 
